8d0afd9c94347fb1b72b5ef2350b42da310e08ac
jessiehuff <huffjd1@gmail.com>
2018-06-15 14:55:18 -0400

    Added Temporary NavLink

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,11 @@ import EventsPage from '../containers/EventsPage';
 const App = (props) => 
   <Router> 
     <div> 
+      <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}> 
+        <NavLink style={{ marginRight: '10px' }} to="/events">See All Events </NavLink> 
+        <NavLink style={{ marginRight: '10px' }} to="/events/new">Add an Event
+        </NavLink> 
+      </div> 
       <Route path="/" render={() => <h1> Welcome to Eventure </h1>} /> 
       <Route path="/events" component={EventsPage} /> 
     </div> 
