7e0d2e77d43d30dd3c479b404d6c8430f47abece
jessiehuff <huffjd1@gmail.com>
2018-06-16 15:23:05 -0400

    Fixed new event route and added Home link to NavLink

--- src/components/App.js
+++ src/components/App.js
@@ -10,12 +10,15 @@ const App = (props) =>
   <Router> 
     <div> 
       <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}> 
+        <NavLink style={{ marginRight: '10px' }} to="/">Home
+        </NavLink> 
         <NavLink style={{ marginRight: '10px' }} to="/events">See All Events </NavLink> 
         <NavLink style={{ marginRight: '10px' }} to="/events/new">Add an Event
         </NavLink> 
       </div> 
       <Route exact path="/" component={Homepage} /> 
       <Route exact path="/events" component={EventsPage} /> 
+      <Route exact path="/events/new" component={EventsNew} /> 
       <Route exact path="/signup" component={Signup} /> 
       <Route exact path="/login" component={Login} /> 
     </div> 
