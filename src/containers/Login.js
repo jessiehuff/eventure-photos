1b9aafb33d96c7a99ceff24acd41c07e75628d48
jessiehuff <huffjd1@gmail.com>
2018-06-16 13:46:25 -0400

    Completed Login Form

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -20,4 +20,49 @@ class Login extends Component {
   handleSubmit(e){
     e.preventDefault(); 
   }
+
+  render(){
+    return(
+      <div> 
+        <Col md={4}></Col> 
+        <Col md={4}> 
+          <div className="loginForm"> 
+            <h1>Log In</h1> 
+            <Form onSubmit={this.handleSubmit}> 
+              <input 
+                label="Email"
+                type="text" 
+                name="email"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              /> 
+              <br></br><br></br>
+              <input
+                label="Password"
+                type="password"
+                name="password"
+                placeholder="Password" 
+                value={this.state.password}
+                onChange={this.handleChange}
+              /> 
+              <br></br><br></br>
+              <div className="submission"> 
+                <Button type="submit" value="Sign Up">Sign Up</Button> 
+                <Button type="link">Forgot Password?</Button> 
+              </div> 
+              <div className="newAccount"> 
+                <hr></hr> 
+                <p>New to Eventure Photos?</p> 
+                <Button href="/signup" bsStyle="primary" bsSize="large" block>Sign Up</Button> 
+              </div> 
+            </Form> 
+          </div> 
+        </Col> 
+        <Col md={4}></Col> 
+      </div> 
+    )
+  }
 }
+
+export default Login; 
