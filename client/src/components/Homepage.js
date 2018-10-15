295a1ff83e53542f10aff45666a4a70795ccf58e
Jessie Huff <huffjd1@gmail.com>
2018-10-15 19:04:37 -0400

    Edited homepage buttons

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -4,11 +4,17 @@ const Homepage = props => {
   return(
     <div className="homepage"> <br></br><br></br><br></br><br></br>
       <div className="landing-option">
-        <h2>Ready to join?</h2>
-        <a className="btn btn-primary" href="/signup" role="button">Sign Up</a>
         <br></br><br></br>
-        <h2>Already a user?</h2>
-        <a className="btn btn-primary" href="/login" role="button">Login</a> 
+        <br></br><br></br>
+        <h2>Have a New Event?</h2>
+        <a className="btn btn-primary" href="/events/new" role="button">New Event</a>
+        <br></br><br></br>
+        <br></br><br></br>
+        <br></br><br></br>
+        <br></br><br></br>
+        <br></br><br></br>
+        <h2>See Existing Events</h2>
+        <a className="btn btn-primary" href="/events" role="button">Current Events</a> 
       </div> 
       <br></br><br></br><br></br><br></br><br></br>
       <br></br><br></br><br></br><br></br><br></br>
