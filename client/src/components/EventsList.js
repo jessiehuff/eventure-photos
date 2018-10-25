4fe64b905479f9e01b9c09d73ec8b3a1eebd68b7
Jessie Huff <huffjd1@gmail.com>
2018-10-25 11:02:25 -0400

    Added cover photo to event listing

--- client/src/components/EventsList.js
+++ client/src/components/EventsList.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 const EventsList = props => {
   const eventListing = props.events
   const renderEvents = eventListing.map((event, index) => 
-     <h5><li key={index}><Link to={`/events/${event.id}`}>{event.name}</Link></li></h5> 
+     <h5><li key={index}><Link to={`/events/${event.id}`}>
+     <img src={event.cover_url} alt="event cover" />
+     <br></br>
+     {event.name}</Link></li></h5>
    );
  
   return (
